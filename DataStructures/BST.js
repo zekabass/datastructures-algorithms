5f6eedb0000e6acc35d2f89ee22a109cffa2690d
zekabass <zekabass@gmail.com>
2018-03-30 16:08:54 +0200

    BST module update

--- DataStructures/BST.js
+++ DataStructures/BST.js
@@ -47,6 +47,37 @@ module.exports = function(){
             inOrderTraverseNode(node.right, callback);
         }
     };
+
+    this.contains = function(value, callback){
+        containsNode(value).then((res)=>{
+            callback(res)
+        })
+    }
+
+    let containsNode = function(value, node) {
+        return new Promise((resolve) => {    
+            function check(value, node) {
+                if(!node) node = root;
+        
+                if(value == node.key) {
+                    resolve(true)
+                }
+
+                if(value < node.key) {
+                    if(!node.left) {
+                        resolve(false)
+                    }
+                    check(value, node.left)
+                } else {
+                    if(!node.right) {
+                        resolve(false)
+                    }
+                    check(value, node.right)
+                }
+            }
+            check(value, node)
+        })
+    }
 }
 
   
